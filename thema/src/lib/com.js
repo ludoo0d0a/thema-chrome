b69c28392ffe99eebc7f8b985a97d1f098823616
ludovic.valente <ludovic.valente@gmail.com>
2010-06-25 11:02:24 +0000

    1st version

---
+++ thema/src/lib/com.js
@@ -0,0 +1,52 @@
+var isChrome = (typeof chrome !== 'undefined'), cbo, profiles = [];
+
+function req(message, cb, data){
+    if (isChrome && chrome.extension) {
+        var o = data || {};
+        o.message = message;
+        chrome.extension.sendRequest(o, cb);
+    }     
+}
+
+/*
+ function inject(message, cb, data){
+ chrome.tabs.getSelected(null, function(tab){
+ var bkg = chrome.extension.getBackgroundPage();
+ chrome.tabs.executeScript(tab.id, 'scanner.js', funtion(){
+ bkg.storage.getItem('scanner-values');
+ });
+ });
+ }*/
+function inject(msg, cb, options){
+    chrome.tabs.getSelected(null, function(tab){
+        if (!isUrl(tab.url)) 
+            return;
+        chrome.tabs.connect(tab.id).postMessage({
+            message: msg,
+            options: options
+        });
+    });
+}
+
+
+function executeScript(file){
+    chrome.tabs.getSelected(null, function(tab){
+        if (!isUrl(tab.url)) 
+            return;
+        var js_file = {};
+        js_file.allFrames = true;
+        js_file.file = file;
+        chrome.tabs.executeScript(tab.id, js_file);
+    });
+}
+
+function isUrl(url){
+    return (new String(url)).match(/^https?:\/\//i) ? true : false;
+}
+
+//http://snipplr.com/view/9649/escape-regular-expression-characters-in-string/
+//http://simonwillison.net/2006/Jan/20/escape/
+var re_encodeRE = new RegExp("[.*+?|()\\[\\]{}\\\\]", "g"); // .*+?|()[]{}\
+function encodeRE(s){
+    return s.replace(re_encodeRE, "\\$&").replace(' ', '\\W');
+}
---
+++ thema/src/scanner.js
@@ -0,0 +1,130 @@
+req('dataprofiles', function(profiles){
+	var url = window.location.href;
+	console.log(url);
+	console.log(profiles);
+	$.each(profiles, function(id, p){
+		if (p && p.url){
+			var re= new RegExp(encodeRE(p.url));
+			if (re.test(url)){
+				console.log('Apply profile '+id + ' / '+p.url);
+				apply(p);
+			}
+		}
+	});
+});
+
+chrome.extension.onConnect.addListener(function(port){
+    port.onMessage.addListener(function(a){
+        if (a.message === 'scan') {
+            savepage(a.options, function(res){
+                res.message = 'bg-scan';
+                chrome.extension.sendRequest(res);
+            });
+        }else if (a.message === 'apply') {
+            apply(a.options.data, function(res){
+                res.message = 'bg-apply';
+                chrome.extension.sendRequest(res);
+            });
+        }
+    });
+});
+
+
+function apply(options, cb){
+	//console.log('fn scanner.apply');
+	addStyle(options.css, 'thcss'||options.id, true);
+	addScript(options.js, 'thjs'||options.id, true);
+}
+
+function savepage(options, cb){
+	//saveimages(options, cb);
+	savecss(options, cb);
+}
+function saveimages(options, cb){
+	$('img').each(function(i, link){
+	
+	});
+}
+function savecss(options, cb){
+    var urls = [], modulos = 0, allcss = false;
+    
+    $('link[rel="stylesheet"]').each(function(i, link){
+        var url = $(link).attr('href');
+        console.log('original:' + url);
+        url = resolveUrl(url);
+        console.log('resolved:' + url);
+        urls.push(url);
+        ++modulos;
+        $.get(url, function(data){
+            --modulos;
+            console.log('url:' + url);
+            var urlimports = checkImport(url, data);
+            if (urlimports && urlimports.length > 0) {
+				urls = urls.concat(urlimports);
+            }
+            if (allcss && modulos == 0) {
+                cb({
+                    location: window.location.href,
+                    urls: urls
+                });
+            }
+        });
+    });
+    allcss = true;
+}
+
+function resolveUrl(url, urlbase){
+    urlbase = urlbase || window.location.href;
+    //var res = noParameters(url);
+	var res = url;
+	
+    //urlbase should ends with /
+    if (!/\/$/.test(urlbase)) {
+        urlbase += '/';
+    }
+    if (!/^http/.test(res)) {
+        if (/^\//.test(res)) {
+            var n = /^https?:\/\/[^\/]*\//.exec(urlbase);
+            if (n && n[0]) {
+                res = n[0] + res;
+            }
+        }
+        else {
+            //relative
+            res = urlbase + res;
+        }
+        res = cleanback(res);
+    }
+    return res;
+}
+
+function cleanback(url){
+    var res = url;
+    var res = res.replace(/^:\/\//g, '/');
+    var res = res.replace(/(\/[^\/]*\/\.\.)/g, '');
+    return res;
+}
+
+function checkImport(url, data){
+    var urls = [], m;
+    while ((m = /@import[^\(]*\(([^)]*)\)/g.exec(data))) {
+        console.log('import:' + m[1]);
+        
+        var uri = parseUri(url);
+		var urlbase = uri.protocol + '://' + uri.authority + uri.directory;
+        
+        var newurl = resolveUrl(m[1], urlbase);
+        console.log('urlbase:' + urlbase);
+		console.log('resolveUrl:' + newurl);
+        urls.push(newurl);
+    }
+    return urls;
+}
+
+
+//remove parameters
+function noParameters(url){
+	var uri = parseUri(url);
+	var res = uri.protocol + '://' + uri.authority + uri.path;
+	return res;
+}
