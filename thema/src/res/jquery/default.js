25cf0f6e6072781587e083a78d02c37edae2b808
ludovic.valente@gmail.com <ludovic.valente@gmail.com@b2c74f18-bc3c-272c-3c8a-f3a81f9228cf>
2010-06-30 13:30:06 +0000

    masonry sample ok

---
+++ thema/src/res/jquery/default.js
@@ -0,0 +1,13 @@
+/**
+ * tHema script Sample
+ */
+//@require jquery //
+tHema.onReady = function(){
+   console.log('jQuery ' + jQuery.fn.jquery + ' loaded!!');
+   
+   setInterval(function(){
+   	jQuery('img,input,select,textarea').toggle();
+   },2000);
+
+}
+
